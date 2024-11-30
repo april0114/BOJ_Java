9d00dc49b8418dbdb3d91470098853c4cd8cda74
ha kyungrim <sue5520607@skuniv.ac.kr>
2024-11-29 17:47:41 -0800

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N, result=0;
+        N = sc.nextInt();
+        result = N/4;
+
+        for (int i=0; i< result; i++){
+        System.out.printf("long ");
+
+    }
+        System.out.printf("int");}
+
+    }
