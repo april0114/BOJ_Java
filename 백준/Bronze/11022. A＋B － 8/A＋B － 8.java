f8968e444ce936672bc1c9dd8c87901f9a40f208
ha kyungrim <sue5520607@skuniv.ac.kr>
2024-12-11 17:01:39 -0800

    [Bronze V] Title: A+B - 8, Time: 232 ms, Memory: 19048 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+            int A = in.nextInt();
+            int B = in.nextInt();
+            int C= A+B;
+
+            System.out.println("Case #" + i + ": " + A + " + " + B +" = "+ C);
+        }
+
+        in.close();
+    }
+}
