b38f5fbe5e7c3421f05159d2b57bc83430068c80
ha kyungrim <sue5520607@skuniv.ac.kr>
2025-01-10 14:41:33 -0800

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 18592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+
+public class Main {
+    static boolean[] arr = new boolean[1000001];
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        String ascii = sc.next();
+        System.out.println((int) ascii.charAt(0));
+
+        }
+    }
+
