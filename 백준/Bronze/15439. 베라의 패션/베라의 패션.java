5888b5c4f41d4f255b544759f45d10d56e10954e
ha kyungrim <sue5520607@skuniv.ac.kr>
2025-01-20 15:53:46 -0800

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int result = N * (N-1);
+        System.out.println(result);
+
+
+    }
+    }
+
