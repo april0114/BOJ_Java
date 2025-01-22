085cf446fa78f75de1ca4dfe37296bf9c1a41b5a
ha kyungrim <sue5520607@skuniv.ac.kr>
2025-01-22 14:57:30 -0800

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int N = Integer.parseInt(br.readLine());
+      int factorial = 1;
+      for( int i=N; i>0; i--){
+        factorial *=i;
+      }
+        System.out.println(factorial);
+    }
+}
