57ffe24b640598594c2e50d6585a66359bea5966
ha kyungrim <sue5520607@skuniv.ac.kr>
2025-04-08 14:45:57 -0700

    [Silver III] Title: 2×n 타일링, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1; // 공집합(빈 공간을 채우는 경우는 1개로 간주)
+        if (n >= 1) dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
